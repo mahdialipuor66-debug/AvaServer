46c64513e93d347c9e65c9b73c6835a27b8ffa91
mahdialipuor66-debug <mahdialipuor66@gmail.com>
2026-08-07 09:37:57 +0330

    v0.3 Stable
    Shopping Search API

--- api/shopping.py
+++ api/shopping.py
@@ -1,21 +1,37 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 
 router = APIRouter()
 
-@router.get("/shopping")
-def shopping():
+products = [
+    {
+        "title": "برنج ایرانی",
+        "price": 850000
+    },
+    {
+        "title": "تلویزیون سامسونگ",
+        "price": 15000000
+    },
+    {
+        "title": "روغن خوراکی",
+        "price": 75000
+    }
+]
 
-    return [
-        {
-            "title": "برنج ایرانی",
-            "price": 850000
-        },
-        {
-            "title": "تلویزیون سامسونگ",
-            "price": 15000000
-        },
-        {
-            "title": "روغن خوراکی",
-            "price": 75000
-        }
-    ]
+
+@router.get("/shopping")
+def shopping(q: str = Query(default="")):
+
+    if q == "":
+        return products
+
+    result = []
+
+    q = q.strip().lower()
+
+    for product in products:
+
+        if q in product["title"].lower():
+
+            result.append(product)
+
+    return result
